dd0e9b3cdaf7d27904d161a48add637de5d35078
Oleg Anohin <oanohin@outlook.com>
2020-10-18 19:11:03 +0400

    Add rotate to Canvas and EmergencyZone

---
+++ Assets/Scripts/Scissors/EmergencyZone.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmergencyZone : MonoBehaviour
+{
+    [SerializeField] private Scissors _scissors;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<TreeLeave>())
+            _scissors.TryCutAnimationPlay();
+    }
+}
--- Assets/Scripts/Scissors/Scissors.cs
+++ Assets/Scripts/Scissors/Scissors.cs
@@ -1,7 +1,33 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class Scissors : MonoBehaviour
 {
+    private Animator _animator;
+    private bool _isAnimationPlaying = false;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void TryCutAnimationPlay()
+    {
+        if (_isAnimationPlaying == false)
+            StartCoroutine(ActivateCutPlayAnimation());
+    }
+
+    private IEnumerator ActivateCutPlayAnimation()
+    {
+        _animator.Play("Cut");
+        _isAnimationPlaying = true;
+
+        yield return new WaitForSeconds(1f);
+
+        _isAnimationPlaying = false;
+        _animator.Play("Idle");
+    }
 }
