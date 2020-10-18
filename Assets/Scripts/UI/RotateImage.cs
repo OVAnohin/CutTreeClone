8f2a011c7b76012cc8460f9ae271c01bdb279abb
Oleg Anohin <oanohin@outlook.com>
2020-10-18 21:49:46 +0400

    Fix RotateImage

---
+++ Assets/Scripts/UI/RotateImage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class RotateImage : MonoBehaviour
+{
+    [SerializeField] private float _rotateSpeed;
+
+    private RectTransform _rectTransform;
+    private float _deltaRotate = 0;
+
+    private void Start()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        if (_deltaRotate >= 1 || _deltaRotate <= -1)
+            _rotateSpeed *= -1;
+
+        _deltaRotate += _rotateSpeed;
+        _rectTransform.localScale = new Vector3(_deltaRotate, 1, 1);
+    }
+}
