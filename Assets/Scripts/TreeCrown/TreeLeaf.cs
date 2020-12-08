13814ec72331d9c6b23db58667861759903feb9e
Oleg Anohin <oanohin@outlook.com>
2020-12-08 22:27:11 +0400

    Add Particle Effects, drop leaf

--- Assets/Scripts/TreeCrown/TreeLeaf.cs
+++ Assets/Scripts/TreeCrown/TreeLeaf.cs
@@ -5,6 +5,8 @@ using UnityEngine.Events;
 
 public abstract class TreeLeaf : MonoBehaviour
 {
+    [SerializeField] private GameObject _dropEffect;
+
     public event UnityAction<TreeLeaf> Clipped;
 
     protected void OnTriggerEnter2D(Collider2D collision)
@@ -12,7 +14,16 @@ public abstract class TreeLeaf : MonoBehaviour
         if (collision.GetComponent<Scissors>())
         {
             Clipped?.Invoke(this);
+
+            ShowDrop();
+
             gameObject.SetActive(false);
         }
+    }
+
+    public void ShowDrop()
+    {
+        Vector2 pos = new Vector2(transform.position.x, transform.position.y);
+        Instantiate(_dropEffect, pos, Quaternion.identity);
     }
 }
